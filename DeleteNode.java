56db1d55bebadc39f0d6eb5b3e734421b6e57ce6
Minhe Zhang <markminhezhang@outlook.com>
2021-04-02 14:08:02 +0800

    Finished 18

---
+++ DeleteNode.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class DeleteNode {
+    public ListNode deleteNode(ListNode head, int val) {
+
+        ListNode sentinel = new ListNode(-1);
+        sentinel.next = head;
+
+        while (sentinel.next != null) {
+            if (sentinel.next.val == val) {
+                sentinel.next = sentinel.next.next;
+            } else {
+                break;
+            }
+        }
+
+        ListNode prev = sentinel;
+        ListNode curr = sentinel.next;
+
+        while (curr != null) {
+            if (curr.val == val) {
+                curr = curr.next;
+                prev.next = curr;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+
+        return sentinel.next;
+    }
+}
